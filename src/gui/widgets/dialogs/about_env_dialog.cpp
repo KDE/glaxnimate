da10ee3e0acf9c3b1ff9a513a5a5986e1d8223b5
Mattia Basaglia <glax@dragon.best>
2023-12-03 18:35:02 +0100

    Show KConfig path on about dialog

--- src/gui/widgets/dialogs/about_env_dialog.cpp
+++ src/gui/widgets/dialogs/about_env_dialog.cpp
@@ -53,8 +53,11 @@ AboutEnvironmentDialog::AboutEnvironmentDialog(QWidget* parent)
 {
     d->setupUi(this);
 
-    // Paths
-    d->line_settings->setText(app::Application::instance()->data_file("settings.ini"));
+    QString config_file = GlaxnimateSettings::self()->config()->name();
+    if ( !QDir::isAbsolutePath(config_file) )
+        config_file = QStandardPaths::locate(QStandardPaths::GenericConfigLocation, config_file, QStandardPaths::LocateFile);
+    d->line_settings->setText(config_file);
+
     d->line_user_data->setText(app::Application::instance()->writable_data_path(""));
     d->line_backup->setText(GlaxnimateApp::instance()->backup_path());
 
