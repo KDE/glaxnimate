b67e80f471ba05b7693def71a65a9bdd31f54872
Mattia Basaglia <glax@dragon.best>
2023-12-04 20:23:31 +0100

    Color theme settings

--- src/gui/widgets/settings/settings_dialog.cpp
+++ src/gui/widgets/settings/settings_dialog.cpp
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2023 Mattia Basaglia <dev@dragon.best>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 #include "settings_dialog.hpp"
 
 #include <QFormLayout>
@@ -7,14 +12,18 @@
 #include <QLineEdit>
 #include <QDir>
 #include <QIcon>
+#include <QComboBox>
+#include <QListView>
+#include <QPushButton>
 
 #include <KConfigSkeleton>
 #include <KLazyLocalizedString>
+#include <KColorSchemeManager>
 
 #include "QtColorWidgets/ColorSelector"
 
+#include "app/widgets/no_close_on_enter.hpp"
 #include "glaxnimate_settings.hpp"
-#include <QComboBox>
 
 using namespace glaxnimate::gui;
 
@@ -44,28 +53,13 @@ public:
     KLazyLocalizedString title;
     KCoreConfigSkeleton* skeleton;
     QFormLayout* layout;
-    KPageWidgetItem* page;
+    KPageWidgetItem* page = nullptr;
 
-    explicit AutoConfigPage(const KLazyLocalizedString& title, const QString& icon, KCoreConfigSkeleton* skeleton, KConfigDialog* parent):
+    explicit AutoConfigPage(const KLazyLocalizedString& title):
         title(title),
-        skeleton(skeleton),
         layout(new QFormLayout(this))
     {
         this->setLayout(layout);
-
-        page = parent->addPage(this, title.toString(), icon);
-    }
-
-    AutoConfigPage& add_item_widget(const char* name, QWidget* widget, const KLazyLocalizedString& label, const KLazyLocalizedString& tooltip = {})
-    {
-        add_item_widget(skeleton->findItem(QString::fromLatin1(name)), widget, label, tooltip);
-        return *this;
-    }
-
-    AutoConfigPage& add_item(const char* name, const KLazyLocalizedString& label, const KLazyLocalizedString& tooltip = {})
-    {
-        add_item(skeleton->findItem(QString::fromLatin1(name)), label, tooltip);
-        return *this;
     }
 
     void add_item(const KConfigSkeletonItem* item, const KLazyLocalizedString& label_text, const KLazyLocalizedString& tooltip)
@@ -127,10 +121,49 @@ public:
             entry.retranslate();
     }
 
-    static AutoConfigPage& builder(const KLazyLocalizedString& title, const QString& icon, KCoreConfigSkeleton* skeleton, KConfigDialog* parent)
+protected:
+    void changeEvent ( QEvent* e ) override
     {
-        auto page = new AutoConfigPage(title, icon, skeleton, parent);
-        return *page;
+        QWidget::changeEvent(e);
+
+        if ( e->type() == QEvent::LanguageChange)
+            retranslate();
+    }
+};
+
+
+
+class AutoConfigBuilder
+{
+public:
+    AutoConfigPage* page;
+    KCoreConfigSkeleton* skeleton;
+    QString icon;
+    KConfigDialog* parent;
+
+    AutoConfigBuilder(const KLazyLocalizedString& title, const QString& icon, KCoreConfigSkeleton* skeleton, KConfigDialog* parent):
+        page(new AutoConfigPage(title)),
+        skeleton(skeleton),
+        icon(icon),
+        parent(parent)
+    {
+    }
+
+    AutoConfigBuilder& add_item_widget(const char* name, QWidget* widget, const KLazyLocalizedString& label, const KLazyLocalizedString& tooltip = {})
+    {
+        page->add_item_widget(skeleton->findItem(QString::fromLatin1(name)), widget, label, tooltip);
+        return *this;
+    }
+
+    AutoConfigBuilder& add_item(const char* name, const KLazyLocalizedString& label, const KLazyLocalizedString& tooltip = {})
+    {
+        page->add_item(skeleton->findItem(QString::fromLatin1(name)), label, tooltip);
+        return *this;
+    }
+
+    void commit()
+    {
+        page->page = parent->addPage(page, page->title.toString(), icon);
     }
 };
 
@@ -169,14 +202,25 @@ QComboBox* combo_from_choices(const QVariantMap& choices)
 
 } // namespace
 
+class SettingsDialog::Private
+{
+public:
+    KColorSchemeManager *color_scheme = nullptr;
+    KPageWidgetItem* color_scheme_page = nullptr;
+    QListView* color_scheme_view = nullptr;
+    app::widgets::NoCloseOnEnter ncoe;
+};
+
 SettingsDialog::SettingsDialog(QWidget *parent)
-: KConfigDialog(parent, QStringLiteral("settings"), GlaxnimateSettings::self())
+    : KConfigDialog(parent, QStringLiteral("settings"), GlaxnimateSettings::self()),
+    d(std::make_unique<Private>())
 {
     resize(940, 700);
+    installEventFilter(&d->ncoe);
 
     KCoreConfigSkeleton* skeleton = GlaxnimateSettings::self();
 
-    AutoConfigPage::builder(kli18nc("Settings", "User Interface"), "preferences-desktop-theme", skeleton, this)
+    AutoConfigBuilder(kli18nc("Settings", "User Interface"), "preferences-desktop-theme", skeleton, this)
         .add_item_widget("icon_theme", combo_from_choices(avail_icon_themes()), kli18n("Icon Theme"))
         .add_item("startup_dialog", kli18n("Show startup dialog"))
         .add_item(
@@ -184,21 +228,61 @@ SettingsDialog::SettingsDialog(QWidget *parent)
             kli18n("Horizontal Timeline Scroll"),
             kli18n("If enabled, the timeline will scroll horizontally by default and vertically with Shift or Alt")
         )
+        .commit()
     ;
 
     auto seconds = new QDoubleSpinBox();
     seconds->setSuffix(i18nc("Seconds suffix", "s"));
 
-    AutoConfigPage::builder(kli18nc("Settings", "New Animation Defaults"), "video-webm", skeleton, this)
+    AutoConfigBuilder(kli18nc("Settings", "New Animation Defaults"), "video-webm", skeleton, this)
         .add_item("width", kli18n("Canvas Width"))
         .add_item("height", kli18n("Canvas Height"))
         .add_item("fps", kli18n("FPS"), kli18n("Frames per secons"))
         .add_item_widget("duration", seconds, kli18n("Duration"), kli18n("Duration in secons"))
+        .commit()
     ;
 
-    AutoConfigPage::builder(kli18nc("Settings", "Open / Save"), "kfloppy", skeleton, this)
+    AutoConfigBuilder(kli18nc("Settings", "Open / Save"), "kfloppy", skeleton, this)
         .add_item("max_recent_files", kli18n("Max Recent Files"), kli18n("Number of items to show in the recent files menu"))
         .add_item("backup_frequency", kli18n("Backup Frequency"), kli18n("Number of items to show in the recent files menu"))
         .add_item("use_native_io_dialog", kli18n("Use system file dialog"))
+        .commit()
     ;
+
+    d->color_scheme = new KColorSchemeManager(this);
+    d->color_scheme_view = new QListView();
+    d->color_scheme_view->setModel(d->color_scheme->model());
+    d->color_scheme_page = addPage(d->color_scheme_view, i18nc("Settings", "Color Scheme"), "preferences-desktop-theme-global");
+    QPushButton *apply = buttonBox()->button(QDialogButtonBox::Apply);
+    connect(d->color_scheme_view->selectionModel(), &QItemSelectionModel::currentChanged, apply, [apply]{apply->setEnabled(true);});
+
+    const QStringList dirPaths =
+        QStandardPaths::locateAll(QStandardPaths::GenericDataLocation, QStringLiteral("color-schemes"), QStandardPaths::LocateDirectory);
+
+    qDebug() <<" ===============";
+    std::map<QString, QString> map;
+    for (const QString &dirPath : dirPaths)
+    {
+        qDebug() << dirPath;
+        const QDir dir(dirPath);
+        const QStringList fileNames = dir.entryList({QStringLiteral("*.colors")});
+        for (const auto &file : fileNames) {
+            qDebug() << file << dir.filePath(file);
+        }
+    }
+
+}
+
+SettingsDialog::~SettingsDialog() = default;
+
+void glaxnimate::gui::SettingsDialog::updateSettings()
+{
+    if ( currentPage() == d->color_scheme_page )
+    {
+        d->color_scheme->activateScheme(d->color_scheme_view->currentIndex());
+    }
+    else
+    {
+        KConfigDialog::updateSettings();
+    }
 }
--- src/gui/widgets/settings/settings_dialog.hpp
+++ src/gui/widgets/settings/settings_dialog.hpp
@@ -1,6 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2023 Mattia Basaglia <dev@dragon.best>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #ifndef SETTINGSDIALOG_H
 #define SETTINGSDIALOG_H
 
+#include <memory>
 #include <KConfigDialog>
 
 namespace glaxnimate::gui {
@@ -11,9 +18,14 @@ class SettingsDialog : public KConfigDialog
 
 public:
     SettingsDialog(QWidget *parent);
+    ~SettingsDialog();
+
+protected:
+    void updateSettings() override;
 
 private:
-    // class AutoConfigPage;
+    class Private;
+    std::unique_ptr<Private> d;
 };
 
 } // namespace glaxnimate::gui
