8a3bacd7ee062d91270ca0495ec85534a10a74ef
Mattia "Glax" Basaglia <dev@dragon.best>
2026-02-16 14:44:03 +0100

    Connect paste action from the keyframe menu

--- src/gui/widgets/timeline/compound_timeline_widget.hpp
+++ src/gui/widgets/timeline/compound_timeline_widget.hpp
@@ -66,6 +66,7 @@ private Q_SLOTS:
     void keyframe_action_enter();
     void keyframe_action_exit();
     void copy_keyframe();
+    void paste_keyframe();
     void collapse_index(const QModelIndex& index);
     void expand_index(const QModelIndex& index);
     void click_index ( const QModelIndex& index );
