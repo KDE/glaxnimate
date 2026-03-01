b5b54f437da9d37aee8baabb88777f990929880d
Mattia "Glax" Basaglia <dev@dragon.best>
2026-03-01 15:12:37 +0100

    CI job to test the minimal library can build

--- lib-example/main.cpp
+++ lib-example/main.cpp
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2026 Mattia Basaglia <dev@dragon.best>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 #include <iostream>
 #include "io/io_registry.hpp"
 #include "renderer/renderer.hpp"
